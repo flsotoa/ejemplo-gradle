b8964d465c2df686b038b26d1b3a423184a03d61
Flavio Soto Aburto <67344555+flsotoa@users.noreply.github.com>
2020-12-17 20:47:21 -0300

    Update maven.groovy

--- maven.groovy
+++ maven.groovy
@@ -28,7 +28,7 @@ def call(){
                      sh 'sleep 200'
 		}
 	    stage('Testing_aplication') {
-                        sh 'curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing'
+                        sh 'curl -X GET http://localhost:8085/rest/mscovid/test?msg=testing'
                     }
             stage("Upload Nexus") {
                     nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/nicolas/code/estudios/usach/unidad3/forks/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
