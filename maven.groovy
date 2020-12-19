3e77c7b77e2e400b2bc6e36bdeab13c9919e83ba
Flavio Soto Aburto <67344555+flsotoa@users.noreply.github.com>
2020-12-19 17:52:27 -0300

    Update maven.groovy
    se agrega env.TAREA =env.STAGE_NAME variables de entorno

--- maven.groovy
+++ maven.groovy
@@ -7,31 +7,39 @@
 
 def call(){
             stage("Build") {
+		    env.TAREA =env.STAGE_NAME
                     sh 'mvn clean compile -e'
             }
             stage("Test") {
+		env.TAREA =env.STAGE_NAME
                 sh 'mvn clean test -e'
             
             }
             stage("Jar") {
-				sh 'mvn.cmd clean package -e'
+		env.TAREA =env.STAGE_NAME
+		sh 'mvn.cmd clean package -e'
                 
             }
             stage("SonarQube") {
-                     withSonarQubeEnv(installationName: 'sonar-fsa') {
-                     sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'                 }
+		env.TAREA =env.STAGE_NAME
+                withSonarQubeEnv(installationName: 'sonar-fsa') {
+                sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'                 }
              }
 	    stage("Run Jar") {
-                     sh 'mvn spring-boot:run &'
+		env.TAREA =env.STAGE_NAME
+                sh 'mvn spring-boot:run &'
                     }
             stage("Sleep") {
-                     sh 'sleep 200'
+		env.TAREA =env.STAGE_NAME
+                sh 'sleep 200'
 		}
 	    stage("Testing_aplication") {
-                        sh 'curl -X GET http://localhost:8085/rest/mscovid/test?msg=testing'
+		env.TAREA =env.STAGE_NAME
+                sh 'curl -X GET http://localhost:8085/rest/mscovid/test?msg=testing'
                     }
             stage("Upload Nexus") {
-                    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/nicolas/code/estudios/usach/unidad3/forks/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+		env.TAREA =env.STAGE_NAME
+                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/nicolas/code/estudios/usach/unidad3/forks/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
 
             }
 }
