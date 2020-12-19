5be4edcd1b64e3bd752958d362497aa55bee6e2c
Flavio Soto Aburto <67344555+flsotoa@users.noreply.github.com>
2020-12-19 17:50:16 -0300

    Update gradle.groovy
    se agregan env.TAREA =env.STAGE_NAME variables de entorno

--- gradle.groovy
+++ gradle.groovy
@@ -18,16 +18,20 @@ def call(){
 	}
                     }
         stage("Run") {
+		env.TAREA =env.STAGE_NAME
 		sh "gradle bootRun &"
                     }
 	stage("Sleep") {
+		env.TAREA =env.STAGE_NAME
 		 sh "sleep 200"
                   }	
         stage("Tes_Rest") {
+		env.TAREA =env.STAGE_NAME
 		sh "curl -X GET localhost:8085/rest/mscovid/test?msg=testing -O  && dir"
 		sh "sleep 100"
                     }
         stage("uploadNexus") {
+		env.TAREA =env.STAGE_NAME
 		nexusPublisher nexusInstanceId: 'Nexus',
 		nexusRepositoryId: 'test-nexus',
 		packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Flavio\\.jenkins\\workspace\\job-nexus\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
